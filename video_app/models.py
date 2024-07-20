c86d0f6c1d6fdc4338d3f7c4f4d267205a928d8a
Jonathan Lane <admiralorbiter@gmail.com>
2024-07-20 15:48:48 -0500

    comments added

--- video_app/models.py
+++ video_app/models.py
@@ -23,11 +23,12 @@ class Media(models.Model):
     MEDIA_TYPE_CHOICES = (
         ('video', 'Video'),
         ('image', 'Image'),
+        ('comment', 'Comment'),
     )
     session = models.ForeignKey(Session, on_delete=models.CASCADE, related_name='media')
     title = models.CharField(max_length=100)
     description = models.TextField()
-    media_type = models.CharField(max_length=5, choices=MEDIA_TYPE_CHOICES)
+    media_type = models.CharField(max_length=10, choices=MEDIA_TYPE_CHOICES)
     video_file = models.FileField(upload_to='videos/', blank=True, null=True)
     image_file = models.ImageField(upload_to='images/', blank=True, null=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
