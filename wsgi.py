3ab3696986c65f9d68428a07d4f6776121dd690c
Jon Lane <admiralorbiter@gmail.com>
2024-09-04 15:28:40 -0500

    added wsgi.py file

---
+++ wsgi.py
@@ -0,0 +1,19 @@
+# +++++++++++ DJANGO +++++++++++
+# To use your own Django app use code like this:
+import os
+import sys
+
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/myusername/mysite'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+## Uncomment the lines below depending on your Django version
+###### then, for Django >=1.5:
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+###### or, for older Django <=1.4
+#import django.core.handlers.wsgi
+#application = django.core.handlers.wsgi.WSGIHandler()
