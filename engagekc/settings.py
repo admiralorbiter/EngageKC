186f2afd550553a83fb4c20157bc8e63d72ab762
Jon Lane <admiralorbiter@gmail.com>
2024-10-06 18:18:55 -0500

    updated testing database

--- engagekc/settings.py
+++ engagekc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import sys
 from celery.schedules import crontab
 import os
 
@@ -119,6 +120,15 @@ DATABASES = {
     }
 }
 
+# Use a separate test database when running tests
+if 'test' in sys.argv or 'pytest' in sys.argv[0]:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:',
+        }
+    }
+
 AUTH_USER_MODEL = 'video_app.CustomAdmin'
 
 # Password validation
@@ -174,3 +184,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Test-specific settings
+if 'test' in sys.argv or 'pytest' in sys.argv[0]:
+    DEBUG = False
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.MD5PasswordHasher',
+    ]
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'test_media')
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
