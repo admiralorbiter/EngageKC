5d5d5a86077fccddadba777409fab7d8662acaf1
Jonathan Lane <admiralorbiter@gmail.com>
2024-12-16 10:53:36 -0600

    migrations for module 3 to push changes

--- video_app/migrations/0018_session_module.py
+++ video_app/migrations/0018_session_module.py
@@ -13,6 +13,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='session',
             name='module',
-            field=models.CharField(choices=[('general', 'Final Project'), ('3', 'Module 3')], default='general', max_length=20),
+            field=models.CharField(choices=[('general', 'Final Project'), ('3', 'Module 3')], default='3', max_length=20),
         ),
     ]
